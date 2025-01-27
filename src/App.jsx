b54031c4f148d4a09adb6f375c5e347dca5e4a75
najubtiw <tiwepoj111@grassdev.com>
2025-01-27 23:38:48 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Card from './Components/Card.jsx';
+
+
+function App() {
+  return (
+    <>
+      <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Card.jsx
@@ -0,0 +1,26 @@
+
+
+
+function Card (){
+
+    return(
+            <div className="card text-light text-center m-auto py-4 mt-5 d-flex align-items-center justify-content-center " style={{width: '18rem'}}>
+                <img src="assets/images/avatar-jessica.jpeg" className="card-img-top rounded-circle w-25 " alt="..."/>
+                <div className="card-body">
+                    <h4 className="card-title font-weight-bold">Jesssica Randall</h4>
+                    <p className="card-text" id="place">London, United Kingdom</p>
+                    <p className="card-text" id="f-e">"Front-end developer and avid reader."</p>
+                    <a href="#" className="btn w-100">GitHub</a>
+                    <a href="#" className="btn w-100 mt-2 py-2">Frontend Mentor</a>
+                    <a href="#" className="btn w-100 mt-2 py-2">Linkedin</a>
+                    <a href="#" className="btn w-100 mt-2 py-2">Twitter</a>
+                    <a href="#" className="btn w-100 mt-2 py-2">Instagram</a>
+                </div>
+            </div>
+       );
+
+};
+
+
+
+export default Card;
